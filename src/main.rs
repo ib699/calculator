68297d12e3d90cafba2daefd71562c8f12a55664
jack <isedighi06@gmail.com>
2023-10-22 11:49:45 +0330

    finished project!!!!

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 fn main() {
-    println!("What is your name?");
+    println!("Please Enter Your input Equation: ");
     let input: String = read_string();
-    // println!("Your name is: {}", input);
     calculate(&input);
 }
 
@@ -15,13 +14,26 @@ fn read_string() -> String {
 
 fn calculate(input: &str){
     let parse_input: Vec<&str> = input.split_whitespace().collect();
-    
+    let mut operator:[i32;3]=[0,0,0];
+    let mut counter = 0;
+
     for part in parse_input{
-        let operator = 0;
         match part {
-             "*" => println!("{}", part),
-             _ => print!("")
+             "*" => operator[1] = 1,
+             "+" => operator[1] = 2,
+             "/" => operator[1] = 3,
+             "-" => operator[1] = 4,
+             _ => if counter==0 {operator[0]=part.parse().unwrap();}else if counter==2 {operator[2]=part.parse().unwrap();}
         }
+        counter +=1;
     }
 
+    match operator[1] {
+        1 => println!("Answer is: {:?}", operator[0] * operator[2]),
+        2 => println!("Answer is: {:?}", operator[0] + operator[2]),
+        3 => println!("Answer is: {:?}", f64::from(operator[0]) / f64::from(operator[2])),
+        4 => println!("Answer is: {:?}", operator[0] - operator[2]),
+        _ => println!("Wrong input bye!!!!")
+        
+    } 
 }
